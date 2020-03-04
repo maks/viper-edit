9858a3e401f4afccc9045a4fc63655183381e481
Maksim Lin <maks@manichord.com>
2020-03-04 17:21:09 +1100

    update to show correct changelog

--- app/src/main/java/com/manichord/viperedit/util/Build.java
+++ app/src/main/java/com/manichord/viperedit/util/Build.java
@@ -28,13 +28,9 @@ public final class Build {
 
     public static final boolean DEBUG = BuildConfig.DEBUG;
 
-    public static final String SUPPORT_EMAIL = "maskyngames@gmail.com";
-
     public static final String GOOGLE_PLAY_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvpZca3gZSeRTHPMgxM+A1nTXuRL+9NTOWA1VJFs6ytppcO96i9EWQhmtXVUOKRQIgbXvkepxF7ut+JEjrbniQubZvmyBs9DxK7xUN4Zc3ZboQDQdfg2HJmZXzn8+joOfjXdS9WzsW7aaWKIQ8QXgOB1RUm9hdMdAlgKw+cEyp27WOoMK5m2H/i7C0MIO9tEQs3Hn9UTjayzzfy3MY+KDaX3T6oKievegbpyqyt8y4cpVusJC+uQFLa4bHKPtA3MaPUG6kU9tRV/DHrvFV6dOaPuTYCnYJELlGNfeqRUF0Nvb3Sv0U+BUoXgevjrlLdLz1bqgPDibLzaQmmofNXOnVQIDAQAB";
 
     public static final int MAX_FILE_SIZE = 20_000;
-
-    public static final boolean FOR_AMAZON = false;
 
     public static class Links {
 
